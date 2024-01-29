27e501fcfe0eea42dc19a31e12a6d8119fc8ed0a
Coin5555 <Coin5555@email.com>
2024-01-30 06:44:35 +1000

    stake.mode

---
+++ app/models/stake.model.js
@@ -0,0 +1,23 @@
+module.exports = (mongoose) => {
+    var schema = mongoose.Schema(
+      {
+        amount: Number,
+        from: String,
+        type: String,
+        createTime: Number,
+        sender: String
+      },
+      { timestamps: true }
+    );
+  
+    schema.method("toJSON", function () {
+      const { __v, _id, ...object } = this.toObject();
+      console.log(_id);
+      object.id = _id;
+      return object;
+    });
+  
+    const Stake = mongoose.model("stake", schema);
+    return Stake;
+  };
+  
