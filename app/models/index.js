fef3064c0ae0695966b1c59ec5bf157b4f6da06c
coin5555 <andersn0098@gmail.com>
2024-03-27 08:02:43 +1000

    modles index

--- app/models/index.js
+++ app/models/index.js
@@ -8,6 +8,7 @@ db.mongoose = mongoose;
 db.url = dbConfig.url;
 db.stakes = require("./stake.model.js")(mongoose);
 db.unstakes = require("./unstake.model.js")(mongoose);
+db.totals = require("./total.model.js")(mongoose);
 
 
 module.exports = db;
